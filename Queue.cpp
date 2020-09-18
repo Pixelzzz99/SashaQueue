c83ad4684f1a0c419ca42a660735ea0f01f6e2e9
Sherzod <sherzik99@gmail.com>
2020-09-18 17:37:13 +0500

    Добавил несколько методов Контейнера

--- Queue.cpp
+++ Queue.cpp
@@ -16,12 +16,12 @@ Queue::Element::Element(DATA_TYPE content, size_t size)
 
 Queue::Element::~Element() = default;
 
-Queue::Element* Queue::Element::getNext()
+Queue::Element *Queue::Element::getNext()
 {
     return _next;
 }
 
-void Queue::Element::setNext(Element* next)
+void Queue::Element::setNext(Element *next)
 {
     this->_next = next;
 }
@@ -46,12 +46,75 @@ void Queue::Element::setSize(size_t size)
     this->_size = size;
 }
 
-void* Queue::alloc_OURMemORY(size_t size)
-{      
+Queue::Iterator::Iterator()
+{
+    this->curElement = nullptr;
+}
+
+Queue::Iterator::Iterator(Element *element)
+{
+    this->curElement = element;
+}
+
+Queue::Iterator::~Iterator() = default;
+
+Queue::Element* Queue::Iterator::getCurElement()
+{
+    return this->curElement;
+}
+
+void Queue::Iterator::setCurElement(Element* element)
+{
+    this->curElement = element;
+}
+
+Queue::DATA_TYPE Queue::Iterator::getElement(size_t &size)
+{
+    if (curElement == nullptr)
+    {
+        throw Container::Error("Can't give element the iterator points to an empty element");
+    }
+    size = curElement->getSize();
+    return curElement->getContent();    
+}
+
+void Queue::Iterator::goToNext()
+{
+    if (!hasNext())
+    {
+        throw Container::Error("no next element");
+    }
+    curElement->getNext();
+} 
+
+bool Queue::Iterator::hasNext()
+{
+    if (curElement == nullptr)
+    {
+        return false;
+    }  
+    else if (curElement->getNext() == nullptr)
+    {
+        return false;
+    }
+    return true;    
+}
+
+bool Queue::Iterator::equals(Container::Iterator* right)
+{
+    if ((dynamic_cast<Iterator*>(right)->curElement == this->curElement))
+    {
+        return true;
+    }
+    return false;    
+}
+
+void *Queue::allocMem(size_t size)
+{
     return _memory.allocMem(size);
 }
 
-void Queue::freeMem(void* ptr)
+void Queue::freeMem(void *ptr)
 {
     _memory.freeMem(ptr);
 }
@@ -61,10 +124,10 @@ void Queue::sizeChange(int size)
     this->_size += size;
 }
 
-Queue::Queue(MemoryManager& memory) : AbstractQueue(memory)
+Queue::Queue(MemoryManager &memory) : AbstractQueue(memory)
 {
     allocMem(sizeof(Element));
-    Element* newElement = new Element();
+    Element *newElement = new Element();
     this->_head = newElement;
     this->_tail = this->_head;
     this->_memory = memory;
@@ -73,27 +136,27 @@ Queue::Queue(MemoryManager& memory) : AbstractQueue(memory)
 
 Queue::~Queue()
 {
-    Queue::Clear();
+    Queue::clear();
 }
 
-int Queue::push(void* elem, size_t size)
+int Queue::push(void *elem, size_t size)
 {
     if (_size == 0)
-    {   
-        //add error if size > maxSize  
+    {
+        //add error if size > maxSize
         allocMem(sizeof(Element));
-        Element* newElement = new Element(elem, size);
+        Element *newElement = new Element(elem, size);
         this->_tail = newElement;
-        this->_head = this->_tail;        
+        this->_head = this->_tail;
     }
     else
     {
-        //add error if size > maxSize  
+        //add error if size > maxSize
         allocMem(sizeof(Element));
-        Element* newElement = new Element(elem, size);
+        Element *newElement = new Element(elem, size);
         this->_tail->setNext(newElement);
-        this->_tail = _tail->getNext();        
-    }    
+        this->_tail = _tail->getNext();
+    }
     sizeChange(1);
     return 0;
 }
@@ -103,22 +166,65 @@ int Queue::pop()
     if (this->_head == nullptr)
     {
         return 1;
-    }    
+    }
 
-    Element* newHead = _head->getNext();
-    freeMem((this->_head)->getSize());
+    Element *newHead = _head->getNext();
+    freeMem(this->_head);
     delete this->_head;
-    this->_head = newHead;   
+    this->_head = newHead;
     sizeChange(-1);
     return 0;
 }
 
-void* Queue::front()
+void *Queue::front(size_t &size)
 {
+    size = this->_head->getSize();
     return this->_head->getContent();
 }
 
-void* Queue::back()
+void *Queue::back(size_t &size)
 {
+    size = this->_tail->getSize();
     return this->_tail->getContent();
+}
+
+int Queue::insert(AbstractQueue::Iterator* iter, DATA_TYPE elem, size_t size)
+{
+    auto iterator = dynamic_cast<Iterator*>(iter);
+    if ((iterator->getCurElement() == nullptr))
+    {
+        return 1;
+    }
+
+    Element* catcherElem = iterator->getCurElement();
+    allocMem(sizeof(Element));
+    auto newElement = new Element(elem, size);
+    Element* oldNextElement = catcherElem->getNext();
+    catcherElem->setNext(newElement);
+    newElement->setNext(oldNextElement);    
+    iterator->setCurElement(newElement);
+    sizeChange(1);
+    return 0;
+}
+
+void Queue::clear()
+{
+    if (this->empty())
+    {
+        return;
+    }
+
+    while (_head != nullptr)
+    {
+        pop();
+    }   
+}
+
+bool Queue::empty()
+{
+    if (_size == 0)
+    {
+        return true;
+    }
+    return false;
 }
--- Queue.h
+++ Queue.h
@@ -8,90 +8,82 @@ private:
     class Element
     {
     private:
-        Element* _next;
+        Element *_next;
         DATA_TYPE _content;
         size_t _size;
 
     public:
         Element();
-        Element(DATA_TYPE content, size_t size);        
+        Element(DATA_TYPE content, size_t size);
         ~Element();
-        Element* getNext();
-        void setNext(Element* next);
+        Element *getNext();
+        void setNext(Element *next);
         DATA_TYPE getContent();
         void setContent(DATA_TYPE content);
         size_t getSize();
         void setSize(size_t size);
     };
 
-    Element* _head;
-    Element* _tail;
+    Element *_head;
+    Element *_tail;
     size_t _size;
 
-    void* allocMem(size_t size);
-    void freeMem(void* ptr);
+    void *allocMem(size_t size);
+    void freeMem(void *ptr);
     void sizeChange(int size);
-    
-public:
 
-    Queue(MemoryManager& mem);
+public:
+    class Iterator : public AbstractQueue::Iterator
+    {
+    private:
+        Element *curElement;
+        Element *step(Element *cur);
+
+    public:
+        Iterator();
+        Iterator(Element *element);
+        ~Iterator();
+
+        Element* getCurElement();
+        void setCurElement(Element *element);
+
+        DATA_TYPE getElement(size_t &size) override;
+        void goToNext() override;
+        bool hasNext() override;
+        bool equals(Container::Iterator *right) override;
+    };
+
+    Queue(MemoryManager &mem);
 
     ~Queue();
 
-    int push(void *elem, size_t size) override;    
+    int push(void *elem, size_t size) override;
 
-    int pop() override;    
+    int pop() override;
 
-    void* front(size_t &size) override;
+    void *front(size_t &size) override;
+
+    void *back(size_t &size) override;
+
+    int insert(AbstractQueue::Iterator *iter, DATA_TYPE elem, size_t size) override;
+
+    int size() override;
+
+    size_t max_bytes() override;
+
+    Iterator* find(void *elem, size_t size) override;
+
+    Iterator* newIterator() override;
+
+    Iterator* begin() override;
+
+    Iterator* end() override;
+
+    void remove(Container::Iterator *iter) override;
+
+    void clear() override;
     
-    void* back(size_t &size) override;
-
-    int insert(Iterator* iter, void* elem, size_t size) override;
-
-    bool empty() override;
-    {
-        if (this->head == nullptr)
-            return 0;
-        if (this->size == 0)
-            return 0;
-        return 1;
-    }
-
-    class Iterator
-    {
-        queue<DATA_TYPE> *currQueue;
-        Element *currElem;
-
-        Iterator(queue *currQue, Element *elem)
-        {
-            this->currQueue = currQue;
-            this->currElem = elem;
-        }
-
-        ~Iterator(){};
-
-        void goToNext()
-        {
-            this->currElem = currelem->next;
-        }
-
-        DATA_TYPE &get()
-        {
-            return this->currElem->content;
-        }
-
-        bool equal(Iterator *iter)
-        {
-            if (this->currQueue != iter->currQueue)
-                return 0;
-            if (this->currElem != iter->currElem)
-                return 0;
-            return 1;
-        }
-    };
-    void insert(Iterator *iter, DATA_TYPE content)
-    {
-        //iter->currElem->content = content; ���� ������ ��������
-        Element *insElem = new Element(iter->currElem, content);
-    }
+    bool empty() override;  
 };
+
+
